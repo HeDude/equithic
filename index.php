d94c30465b85699bb6b0cfbb434d96353dc53f58
Rob Slagter <rob@hedude.com>
2019-03-03 17:43:24 +0100

    Started website

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Equithic</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link href="stylesheets/main.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+    <div class="background_of_overlay overlay background_image_page">
+        <div class="wrapper row1">
+            <header id="header" class="hoc clear">
+                <img id="logo" src="images/logo.svg">
+                <nav id="mainav" class="fl_right">
+                    <ul class="clear">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a class="drop" href="#">Navigatie</a>
+                            <ul>
+                                <li><a href="#introductie">Introductie</a></li>
+                                <li><a href="#kenmerken">Kenmerken</a></li>
+                                <li><a href="#ondernemend_leren">Ondernemend leren</a></li>
+                                <li><a href="#footer">Contact</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="https://wiki.equithic.nl" target="_blank">Wiki</a></li>
+                        <li><a href="https://elo.leerparadijs.nl" target="_blank">ELO</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+        <div id="introductie" class="hoc clear">
+            <div class="flexslider basicslider">
+                <ul class="slides">
+                    <li>
+                        <article>
+                            <p>Equithic</p>
+                            <h3 class="heading">Paarden VO</h3>
+                            <p>Een HAVO of VWO-diploma voor paardenliefhebbers</p>
+                            <footer>
+                                <form class="group" method="post" action="#">
+                                    <fieldset>
+                                        <legend>Inschrijven:</legend>
+                                        <input type="email" value="" placeholder="Schrijf je in&hellip;">
+                                        <button class="fa fa-sign-in" type="submit" title="Submit"><em>OK</em></button>
+                                    </fieldset>
+                                </form>
+                            </footer>
+                        </article>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper row3 coloured">
+        <main class="hoc container clear">
+            <div id="kenmerken">
+                <ul class="nospace group">
+                    <li class="one_third first">
+                        <article>
+                            <div>
+                                <h6 class="heading">Omgeving</h6>
+                                <p>Een rustige en natuurlijke locatie om kalm te worden.</p>
+                            </div>
+                            <img src="images/environment.jpg" alt="">
+                            <footer><a href="https://wiki.equithic.nl/index.php/Hoofdpagina">Lees meer</a></footer>
+                        </article>
+                    </li>
+                    <li class="one_third">
+                        <article>
+                            <div>
+                                <h6 class="heading">Passie</h6>
+                                <p>Paarden nodigen uit om naar school te komen en te leren.</p>
+                            </div>
+                            <img src="images/horses.jpg" alt="">
+                            <footer><a href="https://wiki.equithic.nl/index.php/Hoofdpagina">Lees meer</a></footer>
+                        </article>
+                    </li>
+                    <li class="one_third">
+                        <article>
+                            <div>
+                                <h6 class="heading">Gelijke kansen</h6>
+                                <p>Of je nou arm of rijk bent, iedere leerling heeft een paard en spullen.</p>
+                            </div>
+                            <img src="images/ownership.jpg" alt="">
+                            <footer><a href="https://wiki.equithic.nl/index.php/Hoofdpagina">Lees meer</a></footer>
+                        </article>
+                    </li>
+                </ul>
+            </div>
+            <p class="center"><a class="btn inverse" href="https://wiki.equithic.nl/index.php/Hoofdpagina">Online encyclopedie</a></p>
+            <div class="clear"></div>
+        </main>
+    </div>
+    <div id="ondernemend_leren" class="wrapper bgded overlay" style="background-image:url('images/background_2.jpg');">
+        <section class="hoc container clear">
+            <div class="section_title center">
+                <h3 class="heading">Ondernemend leren</h3>
+                <p>Op Equithic mag je het zelf bedenken en bepalen. Als je maar de leerdoelen haalt.</p>
+            </div>
+            <ul class="nospace group center">
+                <li class="one_third first">
+                    <article><a href="#"><i class="btmspace-30 icon fa fa-500px"></i></a>
+                        <h6 class="heading font-x1">Vakken</h6>
+                        <p>Je koppelt bezig zijn met paarden aan vakken en niet andersom [&hellip;]</p>
+                        <footer><a href="https://wiki.equithic.nl/index.php/Hoofdpagina">Lees meer &raquo;</a></footer>
+                    </article>
+                </li>
+                <li class="one_third">
+                    <article><a href="#"><i class="btmspace-30 icon fa fa-envira"></i></a>
+                        <h6 class="heading font-x1">Eigenaarschap</h6>
+                        <p>Zelf creatieve projecten verzinnen die te maken hebben met paarden [&hellip;]</p>
+                        <footer><a href="https://wiki.equithic.nl/index.php/Hoofdpagina">Lees meer &raquo;</a></footer>
+                    </article>
+                </li>
+                <li class="one_third">
+                    <article><a href="#"><i class="btmspace-30 icon fa fa-empire"></i></a>
+                        <h6 class="heading font-x1">Portfolio</h6>
+                        <p>Je laat zien zelf dat je leerdoelen hebt gehaald [&hellip;]</p>
+                        <footer><a href="https://wiki.equithic.nl/index.php/Hoofdpagina">Lees meer &raquo;</a></footer>
+                    </article>
+                </li>
+            </ul>
+        </section>
+    </div>
+    <div class="wrapper row4 bgded overlay" style="background-image:url('images/background_3.jpg');">
+        <footer id="footer" class="hoc clear">
+            <h3 class="heading">Contact</h3>
+            <nav>
+                <ul class="nospace inline pushright uppercase">
+                    <li><i class="fa fa-phone"></i> 0622034603</li>
+                    <li><i class="fa fa-envelope-o"></i> info@equithic.nl</li>
+                </ul>
+            </nav>
+            <ul class="font_awesome_icon clear">
+                <li><a class="font_awesome_icon_facebook" href="https://www.facebook.com/leerparadijs/"><i class="fa fa-facebook"></i></a></li>
+                <li><a class="font_awesome_icon_twitter" href="https://twitter.com/Leerparadijs"><i class="fa fa-twitter"></i></a></li>
+                <li><a class="font_awesome_icon_linkedin" href="https://www.linkedin.com/company/leerparadijs/"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+        </footer>
+    </div>
+    <div class="wrapper row5">
+        <div id="copyright" class="hoc clear">
+            <p class="fl_left">Copyright &copy; 2019 - All Rights Reserved - <a href="https://www.leerparadijs.nl">Leerparadijs</a></p>
+        </div>
+    </div>
+    <a id="jump2top" href="#top"><i class="fa fa-chevron-up"></i></a>
+
+    <script src="scripts/jquery.min.js"></script>
+    <script src="https://www.hedude.com/scripts/jump2top.js"></script>
+    <script src="scripts/jquery.flexslider-min.js"></script>
+
+</body>
+</html>
